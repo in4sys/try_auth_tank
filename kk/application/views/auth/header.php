196df735832924188c8f868661b0205a7eb5825d
ismailbuhary <ismailbuhary@gmail.com>
2013-08-15 15:46:29 -0700

    Initial Commit

---
+++ kk/application/views/auth/header.php
@@ -0,0 +1,11 @@
+ <!doctype html>
+<html lang="en" dir="ltr">
+    <head>
+    	<meta charset="utf-8">
+        <title>Login</title>
+        
+        
+ <link rel="stylesheet" href="/css/main.css" />
+<link rel="stylesheet" type="text/css" href="<? echo base_url();?>/css/all.css">
+</head>
+<header> <?php echo $heading; ?> </header>
